37879a11f04b516657cffb321652e8c469bb6ae5
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-19 17:55:01 +0300

    .

--- lib/definitions/provider.d.ts
+++ lib/definitions/provider.d.ts
@@ -4,5 +4,5 @@ import { Query, QueryResult, DashboardCreateModel, DashboardModel, CreateResult 
 export interface IClientProvider {
     createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
     getMyDashboards(query?: Query): Promise<QueryResult<DashboardModel>>;
-    getSharedDashboards(search?: ISearchDashboards, query?: Query): Promise<QueryResult<DashboardModel>>;
+    searchDashboards(search?: ISearchDashboards, query?: Query): Promise<QueryResult<DashboardModel>>;
 }
