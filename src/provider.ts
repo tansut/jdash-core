7071ea50890ee73bf50354e7f60f3bd695c51699
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-19 16:17:58 +0300

    .

--- src/provider.ts
+++ src/provider.ts
@@ -1,8 +1,9 @@
 import { Query, QueryResult, DashboardCreateModel, DashboardModel, Metadata, CreateResult } from './models';
 
 
-export interface IJDashProvider {
+export interface IClientProvider {
     getDashboardsOfUser(username: string, query?: Query): Promise<QueryResult<DashboardModel>>;
     getDashboard(id: string): Promise<DashboardModel>;
     createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
+    getMyDashboards(query?: Query): Promise<QueryResult<DashboardModel>>;
 }
