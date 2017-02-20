70a9342b0019b070edc7ca3a146e8ed55c50f356
Tansu TURKOGLU <tansu@Tansus-MacBook-Pro.local>
2017-02-21 02:02:39 +0300

    .

--- lib/definitions/index.d.ts
+++ lib/definitions/index.d.ts
@@ -112,23 +112,3 @@ export interface IClientProvider {
     deleteDashlet(id: string): Promise<any>;
     saveDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
 }
-export interface ITokenProvider {
-    apikey(): string;
-    userToken(): string;
-}
-export declare class JDashProvider implements IClientProvider {
-    private tokenProvider;
-    static getUrl(): string;
-    constructor(tokenProvider: ITokenProvider);
-    private request();
-    getDashboard(id: string): Promise<DashboardModel>;
-    createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
-    getMyDashboards(query?: Query): Promise<QueryResult<DashboardModel>>;
-    searchDashboards(search?: ISearchDashboards, query?: Query): Promise<QueryResult<DashboardModel>>;
-    deleteDashboard(id: string): Promise<any>;
-    saveDashboard(id: string, updateValues: DashboardUpdateModel): Promise<any>;
-    createDashlet(model: DashletCreateModel): Promise<CreateResult>;
-    getDashletsOfDashboard(dashboardId: string): Promise<Array<DashletCreateModel>>;
-    deleteDashlet(id: string): Promise<any>;
-    saveDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
-}
--- src/index.ts
+++ src/index.ts
@@ -101,8 +101,6 @@ export interface LayoutModel {
 
 
 
-import * as axios from 'axios';
-
 export interface IClientProvider {
     getDashboard(id: string): Promise<DashboardModel>;
     createDashboard(model: DashboardCreateModel): Promise<CreateResult>;
@@ -117,69 +115,3 @@ export interface IClientProvider {
     saveDashlet(id: string, updateValues: DashletUpdateModel): Promise<any>;
 }
 
-export interface ITokenProvider {
-    apikey(): string;
-    userToken(): string;
-}
-
-export class JDashProvider implements IClientProvider {
-
-    static getUrl() {
-        return 'http://localhost:3000/jdash/api/v1'
-    }
-
-    constructor(private tokenProvider: ITokenProvider) {
-
-    }
-
-    private request(): axios.AxiosInstance {
-        var instance = axios.default.create({
-            baseURL: JDashProvider.getUrl(),
-            headers: { 'Authentication': 'Bearer ' + this.tokenProvider.userToken() }
-        });
-        return instance;
-    }
-
-    getDashboard(id: string): Promise<DashboardModel> {
-        return this.request().get(`/dashboard/${id}`).then(result => result.data);
-    }
-
-    createDashboard(model: DashboardCreateModel): Promise<CreateResult> {
-        return this.request().post(`/dashboard/create`, model).then(result => result.data);
-    }
-
-    getMyDashboards(query?: Query): Promise<QueryResult<DashboardModel>> {
-        return this.request().get(`/dashboard/my`).then(result => result.data);
-    }
-
-    searchDashboards(search?: ISearchDashboards, query?: Query): Promise<QueryResult<DashboardModel>> {
-        return this.request().post(`/dashboard/search`, {
-            search: search,
-            query: query
-        }).then(result => result.data);
-    }
-
-    deleteDashboard(id: string): Promise<any> {
-        return this.request().post(`/dashboard/delete/${id}`).then(result => result.data);
-    }
-
-    saveDashboard(id: string, updateValues: DashboardUpdateModel): Promise<any> {
-        return this.request().post(`/dashboard/save/${id}`, updateValues).then(result => result.data);
-    }
-
-    createDashlet(model: DashletCreateModel): Promise<CreateResult> {
-        return this.request().post(`/dashlet/create`, model).then(result => result.data);
-    }
-
-    getDashletsOfDashboard(dashboardId: string): Promise<Array<DashletCreateModel>> {
-        return this.request().get(`/dashlet/bydashboard/${dashboardId}`).then(result => result.data);
-    }
-
-    deleteDashlet(id: string): Promise<any> {
-        return this.request().post(`/dashlet/delete/${id}`).then(result => result.data);
-    }
-
-    saveDashlet(id: string, updateValues: DashletUpdateModel): Promise<any> {
-        return this.request().post(`/dashlet/save/${id}`, updateValues).then(result => result.data);
-    }
-}
